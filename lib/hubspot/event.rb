0dbd78f3f18e977367bbeeee9d2c99981d25225f
SirNicholas <zozulyak.nick@gmail.com>
2016-03-09 13:52:42 +0200

    add track_and_set_properties method to Events

--- lib/hubspot/event.rb
+++ lib/hubspot/event.rb
@@ -7,6 +7,11 @@ module Hubspot
         params = options.merge(_n: event_id.to_s).stringify_keys
         Hubspot::Connection.track(TRACK_EVENT_URL, params)
       end
+
+      def track_and_set_properties(event_id, contact_email, contact_properties)
+        Hubspot::Contact.create_or_update_by_email!(contact_email, contact_properties)
+        track(event_id)
+      end
     end
   end
 end
