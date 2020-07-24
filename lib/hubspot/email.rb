52946084f729fe38a135e215b50c5d8c481ae166
SirNicholas <zozulyak.nick@gmail.com>
2020-07-24 12:22:16 +0300

    fix body params

--- lib/hubspot/email.rb
+++ lib/hubspot/email.rb
@@ -8,13 +8,14 @@ module Hubspot
     SINGLE_SEND_EMAIL_PATH = '/email/public/v1/singleEmail/send'.freeze
 
     def self.send_email(email_id, message_props, message_json_body = {})
+      post_params = Hubspot::Utils.hash_to_properties(message_json_body, key_name: "name")
       Hubspot::Connection.post_json(
         SINGLE_SEND_EMAIL_PATH,
         params: {},
         body: {
           emailId: email_id,
           message: message_props,
-        }.merge(message_json_body)
+        }.merge(post_params)
       )
     end
   end
